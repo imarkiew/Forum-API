cbbdf53b547c7c97df6b13460962b1999c9987f4
Igor Markiewicz <igomar067@gmail.com>
2020-05-14 17:44:30 +0200

    Added quotation marks for url

--- src/test/curl/add_new_topic.sh
+++ src/test/curl/add_new_topic.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-curl -d @./json/topic.json -H "Content-Type: application/json" -X POST http://localhost:8888/addNewTopic
+curl -d @./json/topic.json -H "Content-Type: application/json" -X POST "http://localhost:8888/addNewTopic"
